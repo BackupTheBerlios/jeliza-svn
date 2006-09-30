2013a8f57d7ada00bb0eefd32680229495b34976
tobiasschulz <tobiasschulz@7469b2d0-5b16-0410-a4fa-b7396f465b4b>
2006-09-30 21:32:28 +0000

    - dummy

--- trunk/setupWortschatz.sh
+++
@@ -1,9 +0,0 @@
-echo Lade Wortschatz-Installationprogramm
-wget http://tobiasschulz.homedns.org/intelligenz/jeliza/getWortschatz.sh
-chmod 0777 *.sh
-echo Lade Wortschatz
-./getWortschatz.sh 2>&1 | tee getWortschatz.log
-echo Wortschatz geladen
-echo Wortschatz ist unter ./gehirn/ zu finden
-
-
--- trunk/start.bat
+++
@@ -1,3 +0,0 @@
-echo Starting Program
-java -cp .;./tsclasses.jar;./org/homedns/tobiasschulz/apps/jeliza JElizaGui
-echo Program halted
