5c8465c3552a97d5bf7ea22bb8957d802960102f
tobischulz <tobischulz@7469b2d0-5b16-0410-a4fa-b7396f465b4b>
2007-03-03 20:32:40 +0000

    - Autoconf und Automake unterstuetzung

--- trunk-2.0/find-all-dependencies.sh
+++
@@ -1,5 +0,0 @@
-sh find-dep-cpps.sh | sed 's@include/Poco/Util/@src/@g' | sed 's@include/Poco/Net/@src/@g' > tmp3
-cat tmp3 | sed 's@include/Poco/@src/@g' | sed 's@h @cpp @g' > tmp2
-cat tmp2 | sort | uniq > tmp
-rm tmp2
-sh tmp
--- trunk-2.0/wait-while-compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-x=0
-while [ "$(ps aux | grep c++ | grep -v grep)n" != "n" ]; do
-	sleep 1
-done
